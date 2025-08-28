fbf992fc6f0dc1b3afaf51ca678dd2a44de6ba2e
karnansp36 <reviewmaster36@gmail.com>
2025-08-28 18:40:10 +0530

    new update

--- my_project/user_test/urls.py
+++ my_project/user_test/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import user_create_view, image_create_view, user_list_view
+from .views import user_create_view, image_create_view, user_list_view, view_pdf
 
 urlpatterns = [
     path('create/', user_create_view, name='user_create'),
     path('image_upload/', image_create_view, name='image_upload'),
     path('explore/', user_list_view, name='user_list'),
+    path('pdf/<int:pk>/', view_pdf, name='view_pdf'),
+
 ]
--- my_project/user_test/views.py
+++ my_project/user_test/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .forms import UserForm, ImageForm
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, FileResponse, Http404
 from django.contrib import messages
+from .forms import UserForm, ImageForm
 from .models import User_db, image_storage
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 
 def user_create_view(request):
     if request.method == 'POST':
@@ -12,11 +13,11 @@ def user_create_view(request):
             form.save()
             messages.success(request, "User created successfully")
         else:
-            HttpResponse("Form is not valid")
             messages.error(request, "Error in form submission")
     else:
         form = UserForm()
-    return render(request , 'user_db.html',{"form":form})
+    return render(request, 'user_db.html', {"form": form})
+
 
 def image_create_view(request):
     if request.method == 'POST':
@@ -26,10 +27,22 @@ def image_create_view(request):
             messages.success(request, "Image uploaded successfully")
         else:
             messages.error(request, "Error in image upload")
+    else:
+        form = ImageForm()
+    return render(request, 'image_upload.html', {"form": form})
 
-    return render(request, 'image_upload.html', {"form": ImageForm()})
 
 def user_list_view(request):
     images = image_storage.objects.all()
-    return render(request, 'insta_post.html',{"images":images})
+    return render(request, 'insta_post.html', {"images": images})
 
+
+@xframe_options_exempt
+def view_pdf(request, pk):
+    obj = get_object_or_404(image_storage, pk=pk)
+    if not obj.files:
+        raise Http404("No file found")
+
+    response = FileResponse(open(obj.files.path, "rb"), content_type="application/pdf")
+    response["Content-Disposition"] = 'inline; filename="%s"' % obj.files.name
+    return response
